2310661933e77d29669307d609de91d0f9a271df
Karimi-F <fridah37karimi@gmail.com>
2024-12-05 08:44:38 +0300

    modes

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(write_file, file_content):
+    write_file = f"{write_file}.txt" 
+    with open(write_file, 'w') as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(append_file, append_content):
+    append_file = f"{append_file}.txt"
+    with open(append_file, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name = f"{file_name}.txt"
+    with open (file_name, 'r') as file:
+        return file.read()
